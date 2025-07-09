831305b7658fe40462a5e1af1b3ef0e5d1c89a9b
Caspar van Leeuwen <casparl@tcn96.local.snellius.surf.nl>
2025-07-09 14:05:32 +0200

    Conditionally add the accelerator path, only if it's non-empty

--- create_tarball.sh
+++ create_tarball.sh
@@ -62,8 +62,12 @@ if [ -d ${eessi_version}/init ]; then
     find ${eessi_version}/init -type f \! -name '.wh.*' >> ${files_list}
 fi
 
-# consider both CPU-only and accelerator subdirectories
-for subdir in ${cpu_arch_subdir} ${cpu_arch_subdir}/${accel_subdir}; do
+# consider both CPU-only and accelerator subdirectories (if an accelerator was configured)
+sw_subdirs=${cpu_arch_subdir}
+if [ -n ${accel_subdir} ]; then
+    sw_subdirs="${sw_subdirs} ${cpu_arch_subdir}/${accel_subdir}"
+fi
+for subdir in ${sw_subdirs}; do
 
     if [ -d ${eessi_version}/software/${os}/${subdir}/modules ]; then
         # module files
