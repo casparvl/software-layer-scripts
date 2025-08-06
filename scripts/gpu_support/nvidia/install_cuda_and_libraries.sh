a2f5544c51531bfb5b404b28f6edc9b08aff4af9
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2025-08-06 22:30:32 +0200

    Try to change the subdir in which the CUDA toolkit is installed so that it also doesnt include the CPU microarchitecture

--- scripts/gpu_support/nvidia/install_cuda_and_libraries.sh
+++ scripts/gpu_support/nvidia/install_cuda_and_libraries.sh
@@ -132,8 +132,15 @@ for EASYSTACK_FILE in ${TOPDIR}/easystacks/eessi-*CUDA*.yml; do
 
     # If there is a GPU on the node, the installation path will by default have an
     # accelerator subdirectory. For CUDA and cu*, these are binary installations and
-    # don't care about the target compute capability. Our hooks are aware of this and
-    # therefore expect CUDA to be available under EESSI_SITE_SOFTWARE_PATH
+    # don't care about the target compute capability nor the CPU microarchitecture.
+    # Our hooks are aware of this and therefore expect CUDA to be available under 
+    # something like EESSI_SITE_SOFTWARE_PATH, but then with the CPU micro-architecture
+    # stripped
+    # This sed command will capture everything from the EESSI_SITE_SOFTWARE_PATH up until
+    # the EESSI_SOFTWARE_SUBDIR in a capture group. It will the replace that with the content
+    # of the capture group and then have the EESSI_CPU_FAMILY appended
+    # Thus EESSI_SITE_CPU_FAMILY_PATH is something like /cvmfs/software.eessi.io/host_injections/.../x86_64
+    EESSI_SITE_CPU_FAMILY_PATH=$(echo "$EESSI_SITE_SOFTWARE_PATH" | sed 's/\(.*\)'"$EESSI_SOFTWARE_SUBDIR"'/\1'"$EESSI_CPU_FAMILY"'/')
     export EASYBUILD_INSTALLPATH=$EESSI_SITE_SOFTWARE_PATH
 
     # Install modules in hidden .modules dir to keep track of what was installed before
