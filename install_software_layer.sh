4b686e97ba32bbed6e95601ded5cced158b53340
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2025-08-06 12:43:35 +0200

    More debugging

--- install_software_layer.sh
+++ install_software_layer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "TRYING TO LS FIRST"
 ls -al /cvmfs/software.eessi.io
+echo "TRYING TO WRITE AT THIS LEVEL"
+touch /cvmfs/software.eessi.io/foo
 base_dir=$(dirname $(realpath $0))
 source ${base_dir}/init/eessi_defaults
 $base_dir/run_in_compat_layer_env.sh $base_dir/EESSI-install-software.sh "$@"
