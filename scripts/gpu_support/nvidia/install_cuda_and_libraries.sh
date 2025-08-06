333e00971a25b55e3dd3b59d6230260644dfc071
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2025-08-06 22:56:25 +0200

    Fix sed command

--- scripts/gpu_support/nvidia/install_cuda_and_libraries.sh
+++ scripts/gpu_support/nvidia/install_cuda_and_libraries.sh
@@ -140,7 +140,7 @@ for EASYSTACK_FILE in ${TOPDIR}/easystacks/eessi-*CUDA*.yml; do
     # the EESSI_SOFTWARE_SUBDIR in a capture group. It will the replace that with the content
     # of the capture group and then have the EESSI_CPU_FAMILY appended
     # Thus EESSI_SITE_CPU_FAMILY_PATH is something like /cvmfs/software.eessi.io/host_injections/.../x86_64
-    EESSI_SITE_CPU_FAMILY_PATH=$(echo "$EESSI_SITE_SOFTWARE_PATH" | sed 's/\(.*\)'"$EESSI_SOFTWARE_SUBDIR"'/\1'"$EESSI_CPU_FAMILY"'/')
+    EESSI_SITE_CPU_FAMILY_PATH=$(echo "$EESSI_SITE_SOFTWARE_PATH" | sed 's|\(.*\)'"$EESSI_SOFTWARE_SUBDIR"'|\1'"$EESSI_CPU_FAMILY"'|')
     export EASYBUILD_INSTALLPATH=$EESSI_SITE_SOFTWARE_PATH
 
     # Install modules in hidden .modules dir to keep track of what was installed before
